12907c9335385007fd671206294389b2b042a509
ivannr22 <ivanngudi16@gmail.com>
2024-02-04 19:23:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main className="min-h-screen flex justify-center items-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Hello world!
+        </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
